6708da7ca21f0487721f8c35f4420ed509fec81e
Binadamu ISIYOONEKANA <61555648+binadamu-isiyoonekana@users.noreply.github.com>
2022-08-16 17:22:32 +0200

    Improve latex-action action (#4)
    * Set lualatex as default latex engine
    
    * Fix permissions in entrypoint script
    
    * Improve documentation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Github Actions Collection
 
 This repository contains a bunch of Github actions for building various components and artifacts of the Autonomy project.
+
+## List of Actions
+
+| Name   | Description |
+| ------ | ----------- |
+| [`latex-action`](./latex-action/) | Action used for preparing LaTeX documents |
--- latex-action/entrypoint.sh
+++ latex-action/entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
+shopt -s extglob
 
 # -------------------------------------------------------------------------------------------------
 # FUNCTIONS
@@ -28,23 +29,26 @@ working_directory="$2"
 compiler="$3"
 compiler_options="$4"
 
-if [ -z "$main_file" ]; then
+if [[ -z "$main_file" ]]; then
   error "Input 'main_file' is missing."
 fi
 
 if [[ -z "$compiler" && -z "$compiler_options" ]]; then
-  warn "Input 'compiler' and 'compiler_options' are both empty. Reset them to default values."
+  warn "Input 'compiler' and 'compiler_options' are both empty. Reset to default 'latexmk' and 'lualatex' tandem."
   compiler="latexmk"
-  compiler_options="-pdf -file-line-error -halt-on-error -interaction=nonstopmode -shell-escape"
+  compiler_options="-lualatex -file-line-error -halt-on-error -interaction=nonstopmode -shell-escape"
 fi
 
-if [ -n "$working_directory" ]; then
+if [[ -n "$working_directory" ]]; then
+  if [[ ! -d "$working_directory" ]]; then
+    mkdir -p "$working_directory"
+  fi
   cd "$working_directory"
 fi
 
 # Set Github workspace directory as a secure git repository
 #
 # See: https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#docker-container-filesystem
-git config --global --add safe.directory "/github/workspace"
+sudo git config --global --add safe.directory "/github/workspace"
 
-"$compiler" $compiler_options "$main_file"
+sudo "$compiler" $compiler_options "$main_file"
