ba10275e004315f29e014071737557004f15de24
GuiBL4 <guillaume@sofair.io>
2023-03-30 14:26:00 +0200

    feat(ci): add pull request and inner commit message analysis

--- check-pull-request-action/__tests__/main.test.ts
+++ check-pull-request-action/__tests__/main.test.ts
@@ -3,16 +3,42 @@ import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-import {parse} from '../src/parse'
+import {parsePullMessage, parseCommitMessage} from '../src/parse'
 
 test('Pull request subject does not start with a capital letter', async () => {
-  expect(parse("no capital letter (#10)")).toEqual(false)
+  expect(parsePullMessage('no capital letter (#10)')).toEqual(false)
 })
 
 test('Squash and merge commit with pull request reference', async () => {
-  expect(parse("Commit with pull request reference as (#12)")).toEqual(true)
+  expect(
+    parsePullMessage('Commit with pull request reference as (#12)')
+  ).toEqual(true)
 })
 
 test('Squash and merge commit with pull request reference but not starting with a capital letter', async () => {
-  expect(parse("commit with pull request reference as (#12)")).toEqual(false)
+  expect(
+    parsePullMessage('commit with pull request reference as (#12)')
+  ).toEqual(false)
+})
+
+test("Commit's message doesn't begin by a defined type", async () => {
+  expect(parseCommitMessage('add: some addendum')).toEqual(false)
+})
+
+test("Commit's message doesn't finish by a minuscule letter", async () => {
+  expect(parseCommitMessage('break(scope): add a breakin change.')).toEqual(
+    false
+  )
+})
+
+test("Commit's message is well formed with a scope", async () => {
+  expect(parseCommitMessage('feat(scope): add something')).toEqual(true)
+})
+
+test("Commit's message is well formed without a scope", async () => {
+  expect(parseCommitMessage('refac: add something')).toEqual(true)
+})
+
+test("Commit's message contains a forbiden white space before the :", async () => {
+  expect(parseCommitMessage('feat(scope) : add something')).toEqual(false)
 })
